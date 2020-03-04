036d2c7d4c1722631961baa4008b6e30705735e6
Julio Iván <phantomcide@Quark-3.localdomain>
2020-03-04 06:27:47 -0700

    Layouts nuevos

---
+++ app/src/main/java/garcia/julio/fakeflix/DetallePelicula.kt
@@ -0,0 +1,24 @@
+package garcia.julio.fakeflix
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_detalle_pelicula.*
+
+class DetallePelicula : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalle_pelicula)
+
+        val bundle = intent.extras
+        if(bundle != null){
+            detalle_image.setImageResource(bundle.getInt("header"))
+            detalle_titulo.setText(bundle.getString("titulo"))
+            detalle_sinopsis.setText(bundle.getString("sinopsis"))
+        }
+
+    }
+
+
+
+}
---
+++ app/src/main/java/garcia/julio/fakeflix/Pelicula.kt
@@ -0,0 +1,3 @@
+package garcia.julio.fakeflix
+
+data class Pelicula(var titulo: String, var image: Int, var header: Int, var sinopsis: String)
